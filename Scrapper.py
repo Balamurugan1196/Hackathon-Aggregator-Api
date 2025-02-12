104186e857a842e4e5623c2b540604040c67f4ed
Balamurugan1196 <balamuruganb0207@gmail.com>
2025-02-12 22:59:33 +0530

    Fixed hackathon name extraction using h3 tag

--- Scrapper.py
+++ Scrapper.py
@@ -40,7 +40,7 @@ time.sleep(10)
 
 # ✅ Scroll logic to load more hackathons
 SCROLL_COUNT = 30  # Maximum number of scrolls
-SCROLL_PAUSE_TIME = 6  # Time to wait after each scroll
+SCROLL_PAUSE_TIME = 10  # Time to wait after each scroll
 
 
 for _ in range(SCROLL_COUNT):
